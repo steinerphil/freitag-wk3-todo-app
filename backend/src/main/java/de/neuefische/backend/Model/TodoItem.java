a204aed4ce4b3a48a53ade9081c1b1f0146fbee0
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-09-24 10:04:54 +0200

    add ToDoItem Model

---
+++ backend/src/main/java/de/neuefische/backend/Model/TodoItem.java
@@ -0,0 +1,17 @@
+package de.neuefische.backend.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TodoItem {
+    int id;
+    String title;
+    String status;
+
+
+
+}
