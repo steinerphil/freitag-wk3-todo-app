fece57d55e1e46397f8c843d91f0795f58a96ac3
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-09-24 14:38:05 +0200

    add delete and put test

--- backend/src/test/java/de/neuefische/backend/controller/TodoControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/TodoControllerTest.java
@@ -8,8 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -30,8 +34,6 @@ class TodoControllerTest {
         todoRepo.clear();
     }
 
-
-
     @Test
     void addToDoItemTest() {
         //Given
@@ -45,9 +47,37 @@ class TodoControllerTest {
 
     @Test
     void updateStatus() {
+        //Given
+        String URL="http://localhost:"+port+"/api/todo/1";
+        TodoItem todoItem=new TodoItem(1,"Title","OPEN");
+        todoRepo.add(todoItem);
+        //When
+        HttpEntity<TodoItem> entity=new HttpEntity<>(todoItem);
+        ResponseEntity<TodoItem> response1=testRestTemplate.exchange(URL, HttpMethod.PUT,entity,TodoItem.class);
+
+        //Then
+        assertThat(response1.getStatusCode(), is(HttpStatus.OK));
+
     }
 
     @Test
     void deleteItem() {
+
+      //Given
+        todoRepo.add(new TodoItem("Test1", "OPEN"));
+        todoRepo.add(new TodoItem("Test2", "OPEN"));
+
+
+        String URL="http://localhost:"+port+"/api/todo/1";
+
+        //WHEN
+        testRestTemplate.delete(URL);
+
+        //THEN
+
+        assertThat(todoRepo.getById(2),is(new TodoItem(2, "Test2","OPEN")));
+        assertFalse(todoRepo.list().contains(new TodoItem(1, "Test1", "OPEN")));
     }
-}
+    }
+
+
