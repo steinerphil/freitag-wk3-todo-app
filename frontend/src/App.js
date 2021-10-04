b0e56f3bdf00e912413761c7b30f617a1c381948
Philipp Steiner <philipp-steiner@gmx.de>
2021-10-04 20:58:59 +0200

    multiple changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,57 @@
 import './App.css';
 import KanbanSection from './components/KanbanSection';
-import jsonResponse from './response.json';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
 function App() {
 
-    const [text, setText] = useState([])
+    const [input, setInput] = useState([])
     const [open, setOpen] = useState([]);
     const [progress, setProgress] = useState([]);
     const [done, setDone] = useState([]);
+    const [allItems, setAllItems] = useState([])
 
     const handleInput = (action) => {
         let string = action.target.value;
-        setText(string);
+        setInput(string);
     }
 
     const handleSubmit = () => {
         const newTodo = {
             "status" : "OPEN",
-            "description" : text
+            "description" : input
         }
         axios.post('/api/todo', newTodo)
             .then(r => setOpen([...open, r.data]))
             .catch(console.log)
     }
 
+    useEffect(() => {
+        axios.get('/api/todo').then(response => setAllItems(response.data))
+    }, [])
+
+    useEffect(() => {
+        for (let i = 0; i < allItems.length ; i++) {
+            console.log(allItems[i].status)
+            if(allItems[i].status === "OPEN"){
+                open.push(allItems[i]);
+            }
+            if(allItems[i].status === "IN_PROGRESS"){
+                progress.push(allItems[i])
+            }
+            if(allItems[i].status === "DONE"){
+                done.push(allItems[i])
+            }
+        }
+    }, [allItems])
+
+
+
     function test(){
         console.log(open)
+        console.log(progress)
+        console.log(done)
+        console.log(allItems)
     }
 
     return (
