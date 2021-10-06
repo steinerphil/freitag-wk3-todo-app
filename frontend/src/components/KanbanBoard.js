3aae64bfca466bc0ccae3f2fb8a4eaa5be464bb9
Philipp Steiner <philipp-steiner@gmx.de>
2021-10-06 15:37:48 +0200

    responsive styling

--- frontend/src/components/KanbanBoard.js
+++ frontend/src/components/KanbanBoard.js
@@ -4,14 +4,11 @@ import CreateTodoFields from "./CreateTodoFields";
 
 export default function KanbanBoard({handleSetProgress, handleSetDone, handleDelete, handleInput, handleSubmit, input, open, progress, done}) {
 
-    let {slug} = useParams()
-    slug{
-        
-    }
 
     return(
 
         <Switch>
+
             <Route path="/" exact>
                 <KanbanSection content={open} name="Open" onClick={handleSetProgress}/>
                 <KanbanSection content={progress} name="Progress" onClick={handleSetDone}/>
@@ -21,32 +18,50 @@ export default function KanbanBoard({handleSetProgress, handleSetDone, handleDel
                     handleInput={handleInput}
                     handleSubmit={handleSubmit}/>
             </Route>
+
+
             <Route path="/todo/open">
-                {open.length === 0 && <p>No todos in status open. Create one:</p>}
-                {open.length === 0 && <CreateTodoFields
-                    input={input}
-                    handleInput={handleInput}
-                    handleSubmit={handleSubmit}
-                />}
-                {open.length > 0 && <KanbanSection content={open} name="Open" onClick={handleSetProgress}/>}
-                {open.length > 0 && <p>Create another one:</p>}
-                {open.length > 0 && <CreateTodoFields
-                    input={input}
-                    handleInput={handleInput}
-                    handleSubmit={handleSubmit}
-                />}
+                {open.length === 0 &&
+                    <div>
+                        <p>No todos in status open. Create one:</p>
+                        <CreateTodoFields
+                            input={input}
+                            handleInput={handleInput}
+                            handleSubmit={handleSubmit}
+                        />
+                    </div>
+                }
+                {open.length > 0 &&
+                <div>
+                    <KanbanSection content={open} name="Open" onClick={handleSetProgress}/>
+                    <p>Create another one:</p>
+                    <CreateTodoFields
+                        input={input}
+                        handleInput={handleInput}
+                        handleSubmit={handleSubmit}
+                    />
+                </div>
+                }
             </Route>
+
+
             <Route path="/todo/progress">
                 {progress.length === 0 && <p>No todos in progress, <Link to="/open">grab one!</Link></p>}
                 {progress.length > 0 &&
-                <KanbanSection content={progress} name="Progress" onClick={handleSetDone}/>}
-                {progress.length > 0 && <Link to="/open"> -> grab another one! </Link>}
+                    <div>
+                        <KanbanSection content={progress} name="Progress" onClick={handleSetDone}/>
+                        <Link to="/open"> -> grab another one! </Link>
+                    </div>
+                }
             </Route>
+
+
             <Route path="/todo/done">
                 {done.length === 0 &&
                 <p>Nothing is done today :(<Link to="/progress"> ...finish a progress</Link></p>}
                 {done.length > 0 && <KanbanSection content={done} name="Done" onClick={handleDelete}/>}
             </Route>
+
 
         </Switch>
 
